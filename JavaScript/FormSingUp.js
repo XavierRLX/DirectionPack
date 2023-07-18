df1c291e33aaf409ca246db8b5583c08d80e70be
TemperoDoDivino <91702228+XavierRLX@users.noreply.github.com>
2023-07-17 23:11:56 -0300

    falta a vlidação por user ainda

--- JavaScript/FormSingUp.js
+++ JavaScript/FormSingUp.js
@@ -1,25 +1,8 @@
-const showPasswordButtonMainDois = document.getElementById('showPasswordButtonMainDois');
-const inputPassMainDois = document.getElementById('inputPassMainDois');
-const inputPassMainDoisAgain = document.getElementById('inputPassMainDoisAgain');
 
-showPasswordButtonMainDois.addEventListener('click', function(){
-  if (inputPassMainDois.type === 'password') {
-    inputPassMainDois.type = 'text';
-    inputPassMainDoisAgain.type = 'text'
-    showPasswordButtonMainDois.style.boxShadow = '5px 0px 0px 0px #4ae922';
-    
-  } else {
-    inputPassMainDois.type = 'password';
-    inputPassMainDoisAgain.type = 'password'
-    showPasswordButtonMainDois.style.boxShadow = '5px 0px 0px 0px #e9c422';
-  }
-});
-
-//
 function openModal(message) {
   var modal = document.getElementById("myModal");
   modal.style.display = "block";
-  var modalContent = document.getElementById("modalContent");
+  var modalContent = document.getElementById("avisoForm");
   modalContent.innerText = message;
 }
 
@@ -50,7 +33,6 @@ function validateForm() {
   var passwordLabel = document.getElementById("labelPassMainDois");
   var passwordAgainInput = document.getElementById("inputPassMainDoisAgain");
   var passwordAgainLabel = document.getElementById("labelPassMainDoisAgain");
-  var pModal = document.getElementById("avisoForm");
 
   clearError(nameLabel);
   clearError(lastLameLabel);
@@ -115,46 +97,15 @@ function validateForm() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
 // Função para verificar o usuário no localStorage ao carregar a página
 function checkUserOnLoad() {
   // Recuperar o valor do campo de username
-  var username = document.getElementById("inputEmail").value;
+  var username = document.getElementById("inputName").value;
 
   // Verificar se o usuário existe no localStorage
   var storedUser = localStorage.getItem("user");
+
+
 
   if (storedUser) {
     // Converter a string JSON para objeto
@@ -166,99 +117,118 @@ function checkUserOnLoad() {
       var welcomeMessage = "Welcome " + user.name + " " + user.lastname + "!";
       alert(welcomeMessage);
     }
+    
   }
-}
+}  
 
-// Adicionar um event listener ao carregar a página
-window.addEventListener("load", checkUserOnLoad);
-
-// Função para salvar o usuário no localStorage
-function saveUser() {
-    // Recuperar os valores dos campos
-    var name = document.getElementById("inputNameMainDois").value;
-    var lastname = document.getElementById("inputLastnameMainDois").value;
-    var username = document.getElementById("inputUserNameMainDois").value;
-    var password = document.getElementById("inputPassMainDois").value;
-  
-    // Criar um objeto usuário com os valores dos campos
-    var user = {
-      name: name,
-      lastname: lastname,
-      username: username,
-      password: password
-    };
-  
-    // Converter o objeto para uma string JSON
-    var userJSON = JSON.stringify(user);
-  
-    // Salvar o usuário no localStorage com a chave "user"
-    localStorage.setItem("user", userJSON);
-
-    console.log(user)
-  }
-  
-  // Adicionar um event listener ao botão "Save"
-  var btnSave = document.getElementById("btnSave");
-  btnSave.addEventListener("click", function(event) {
-    event.preventDefault(); // Evitar que o formulário seja enviado
-  
-    // Chamar a função para validar os campos
-    var isValid = validateForm();
-  
-    if (isValid) {
-      // Se os campos forem válidos, chamar a função para salvar o usuário
-      saveUser();
-  
-      // Exibir uma mensagem de sucesso
-      alert("User saved successfully!");
-  
-      // Redirecionar para a página de login
-      desfazerAcoes();
-    }
-  });
-
-// Função para verificar o usuário no localStorage
-function checkUser() {
-    // Recuperar o valor do campo de username
-    var username = document.getElementById("inputEmail").value;
-    var password = document.getElementById("inputPassMainDois").value;
-  
-    // Verificar se o usuário existe no localStorage
+function checkExistingUser(username) {
     var storedUser = localStorage.getItem("user");
   
     if (storedUser) {
-      // Converter a string JSON para objeto
-      var user = JSON.parse(storedUser);
-  
-      // Verificar se o username corresponde ao usuário salvo
-      if (username === user.username) {
-        // Verificar se a senha corresponde à senha do usuário salvo
-        if (password === user.password) {
-          // Exibir mensagem de boas-vindas
-          var welcomeMessage = "Welcome " + user.name + " " + user.lastname + "!";
-          alert(welcomeMessage);
-        } else {
-          // Exibir mensagem de erro de senha incorreta
-          alert("Invalid password");
-        }
-      } else {
-        // Exibir mensagem de erro de username inválido
-        alert("Invalid username");
+      var existingUser = JSON.parse(storedUser);
+      if (username === existingUser.username) {
+        return true; // Usuário já existe
       }
-    } else {
-      // Exibir mensagem de erro de usuário não encontrado
-      alert("User not found");
+    }
+  
+    return false; // Usuário não existe
+  }
+
+
+// Função para salvar o usuário no localStorage
+function saveUser() {
+  // Recuperar os valores dos campos
+  var name = document.getElementById("inputNameMainDois").value;
+  var lastname = document.getElementById("inputLastnameMainDois").value;
+  var username = document.getElementById("inputUserNameMainDois").value;
+  var password = document.getElementById("inputPassMainDois").value;
+
+    // Verificar se o usuário já existe
+    if (checkExistingUser(username)) {
+      alert("Username already exists. Please choose a different username.");
+      return;
+    }
+    
+  // Criar um objeto usuário com os valores dos campos
+  var user = {
+    name: name,
+    lastname: lastname,
+    username: username,
+    password: password
+  };
+
+
+  // Converter o objeto para uma string JSON
+  var userJSON = JSON.stringify(user);
+
+  // Salvar o usuário no localStorage com a chave "user"
+  localStorage.setItem("user", userJSON);
+
+  console.log(user)
+
+    // Atribuir o objeto do usuário à variável global currentUser
+    currentUser = user;
+
+}
+
+
+// Adicionar um event listener ao botão "Save"
+var btnSave = document.getElementById("btnSave");
+btnSave.addEventListener("click", function (event) {
+  event.preventDefault(); // Evitar que o formulário seja enviado
+
+  // Chamar a função para validar os campos
+  var isValid = validateForm();
+
+  if (isValid) {
+    // Se os campos forem válidos, chamar a função para salvar o usuário
+    saveUser();
+
+    // Exibir uma mensagem de sucesso
+    alert("User saved successfully!");
+
+    // Redirecionar para a página de login
+    desfazerAcoes();
+  }
+});
+
+function checkUser() {
+  // Recuperar o valor do campo de username e senha
+  var username = document.getElementById("inputName").value;
+  var password = document.getElementById("inputPass").value;
+
+  // Verificar se existem usuários no localStorage
+  var storedUsers = localStorage.getItem("users");
+  if (storedUsers) {
+    // Converter a string JSON para um array de objetos de usuários
+    var users = JSON.parse(storedUsers);
+
+    // Percorrer todos os usuários
+    for (var i = 0; i < users.length; i++) {
+      var user = users[i];
+
+      // Verificar se o username e a senha correspondem ao usuário atual
+      if (username === user.username && password === user.password) {
+        // Exibir mensagem de boas-vindas
+        var welcomeMessage = "Welcome " + user.name + " " + user.lastname + "!";
+        alert(welcomeMessage);
+        return; // Encerrar a função após encontrar uma correspondência
+      }
     }
   }
 
+  // Se nenhum usuário corresponder, exibir mensagem de erro
+  alert("Invalid username or password");
+}
+
+
+
 // Adicionar um event listener ao formulário de login
 var formLogin = document.getElementById("login");
-formLogin.addEventListener("submit", function(event) {
+formLogin.addEventListener("submit", function (event) {
   event.preventDefault(); // Evitar que o formulário seja enviado
 
   // Chamar a função para verificar o usuário
   checkUser();
 
-  // Redirecionar para a página de boas-vindas (caso necessário)
-  // window.location.href = "welcome.html";
 });
