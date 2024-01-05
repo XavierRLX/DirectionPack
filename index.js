f24877fe90e69f04970c3ec0b9d056ff3d3c8f15
XavierRLX <renanlima2000.aer@gmail.com>
2024-01-05 15:39:52 -0300

    Rotas
    Mudanças nas pastas e atualização das rotas dos arquivos "static".

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; 
-
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser'); 
@@ -11,36 +9,48 @@ app.post('/email/send-email-welcome', emailRoutes);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static('PagLogin'));
-app.use(express.static('PagLoad'));
-app.use(express.static('PagHome'));
+app.use('/javaScript', express.static(__dirname + '/src/javaScript'));
+app.use('/style', express.static(__dirname + '/src/style'));
+app.use('/assets/img', express.static(__dirname + '/src/assets/img'));
 
 
 const path = require('path');
 
 app.get('/', (req, res) => {
-  const indexPath = path.join(__dirname, './PagLoad/index.html');
+  const indexPath = path.join(__dirname, './html/load.html');
   res.sendFile(indexPath);
 });
 
 app.get('/load', (req, res) => {
-  const indexPath = path.join(__dirname, './PagLoad/index.html');
+  const indexPath = path.join(__dirname, './html/load.html');
   res.sendFile(indexPath);
 });
 
 app.get('/login', (req, res) => {
-  const indexPath = path.join(__dirname, './PagLogin/paglogin.html');
+  const indexPath = path.join(__dirname, './html/paglogin.html');
   res.sendFile(indexPath);
 });
 
-app.get('/PagMain/index.html', (req, res) => {
-  const indexPath = path.join(__dirname, './PagHome/index.html');
+app.get('/html/home.html', (req, res) => {
+  const indexPath = path.join(__dirname, './html/home.html');
   res.sendFile(indexPath);
 });
 
 app.get('/home', (req, res) => {
-  const indexPath = path.join(__dirname, './PagHome/index.html');
+  const indexPath = path.join(__dirname, './html/home.html');
   res.sendFile(indexPath);
+});
+
+
+// Middleware para lidar com erros 404
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, './Routes/modelError.html'));
+});
+
+// Middleware de tratamento de erros
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).sendFile(path.join(__dirname, './Routes/modelError.html'));
 });
 
 
