4411c253786c62a8c766bd50fda6e9c73cff5cdb
XavierRLX <renanlima2000.aer@gmail.com>
2024-01-15 11:25:42 -0300

    Verificação de login
    Aqui verifico se o user esta logado ou não

--- src/javaScript/load.js
+++
@@ -1,7 +0,0 @@
-function redirectToLoginPage(){
-    setTimeout(function(){
-        window.location.href = '/login';
-    }, 2000);
-   }
-
-   window.addEventListener('load', redirectToLoginPage);
